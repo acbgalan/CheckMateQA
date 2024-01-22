3fbeec7f68bf6874a5e6c0a8aa58d11d700f7db4
Carlos Blanco <acbgalan@gmail.com>
2024-01-22 23:46:30 +0100

    News data annotations for AppllicationUser class, we limit the maximum sizes of the fields

--- src/CheckMateQA.Models/ApplicationUser.cs
+++ src/CheckMateQA.Models/ApplicationUser.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,12 @@ namespace CheckMateQA.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [DisplayName("Nombre")]
+        [StringLength(50, ErrorMessage = "El campo {0} soporta un máximo de {1} caracteres")]
         public string Name { get; set; }
+
+        [DisplayName("Imagen de perfil")]
+        [StringLength(36, ErrorMessage = "El campo {0} soporta un máximo de {1} caracteres")]
         public string ProfilePicture { get; set; }
     }
 }

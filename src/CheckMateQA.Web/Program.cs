7d28593ff4919f2468cfb5a3c086c5b0918ed44a
Carlos Blanco <acbgalan@gmail.com>
2024-01-22 19:11:18 +0100

    Context for Authentification. Web project reference DataAcces project Authentication is configured as a service in Program.cs

--- src/CheckMateQA.Web/Program.cs
+++ src/CheckMateQA.Web/Program.cs
@@ -1,7 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using CheckMateQA.DataAccess.Data;
+using CheckMateQA.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AuthentificationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Authentification")));
+
+//Identity configuration
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthentificationContext>()
+    .AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => options.LoginPath = "/Account/Login"); ;
 
 var app = builder.Build();
 
@@ -18,6 +31,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(

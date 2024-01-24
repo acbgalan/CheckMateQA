89f73429179fadf12da867ad28786d88e7865c22
Carlos Blanco <acbgalan@gmail.com>
2024-01-24 01:20:57 +0100

    Implementation of the login method.

--- src/CheckMateQA.Services/UserAuthentification.cs
+++ src/CheckMateQA.Services/UserAuthentification.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,7 +27,55 @@ namespace CheckMateQA.Servicess
 
         public async Task<ServiceResponse> LoginAsync(LoginDTO model)
         {
-            throw new NotImplementedException();
+            var srvResponse = new ServiceResponse();
+            var user = await _userManager.FindByNameAsync(model.Username);
+
+            if (user == null)
+            {
+                srvResponse.Success = false;
+                srvResponse.Message = "Usuario no válido";
+                return srvResponse;
+            }
+
+            //we will match password
+            if (!await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                srvResponse.Success = false;
+                srvResponse.Message = "Contraseña no válida";
+                return srvResponse;
+            }
+
+            var signInResult = await _signInManager.PasswordSignInAsync(user, model.Password, false, true);
+
+            if (signInResult.Succeeded)
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user.Name)
+                };
+
+                foreach (var rol in userRoles)
+                {
+                    authClaims.Add(new Claim(ClaimTypes.Role, rol));
+                }
+
+                srvResponse.Success = true;
+                srvResponse.Message = "Sesión iniciada correctamente";
+                return srvResponse;
+            }
+            else if (signInResult.IsLockedOut)
+            {
+                srvResponse.Success = false;
+                srvResponse.Message = "Usuario bloqueado";
+                return srvResponse;
+            }
+            else
+            {
+                srvResponse.Success = false;
+                srvResponse.Message = "Error al iniciar sesión";
+                return srvResponse;
+            }
         }
 
         public async Task<ServiceResponse> LogoutAsync()
@@ -81,7 +130,7 @@ namespace CheckMateQA.Servicess
             }
             else
             {
-                await _userManager.AddToRoleAsync(user, model.Role);                
+                await _userManager.AddToRoleAsync(user, model.Role);
             }
 
             svrResponse.Success = true;

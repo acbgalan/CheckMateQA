3e78b9f1f6b105c383fe2e6564c12f745b5ad6eb
Carlos Blanco <acbgalan@gmail.com>
2024-01-24 02:04:10 +0100

    User registration method. Test

---
+++ src/CheckMateQA.Web/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CheckMateQA.Services.Interfaces;
+using CheckMateQA.Models.DTO;
+using CheckMateQA.Models;
+
+namespace CheckMateQA.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUserAuthentification _userAuthentification;
+
+        public AccountController(IUserAuthentification userAuthentification)
+        {
+            _userAuthentification = userAuthentification;
+        }
+
+
+        public async Task<string> Register()
+        {
+            RegistrationDTO registration = new RegistrationDTO()
+            {
+                Name = "Carlos Blanco",
+                Email = "cblanco@mail.com",
+                Username = "cblanco",
+                Password = "P@ssw0rd!",
+                PasswordConfirm = "ssw0rd!",
+                Role = "Administrator"
+            };
+
+            ServiceResponse result = await _userAuthentification.RegisterAsync(registration);
+
+            if (result.Success)
+            {
+                return $"{DateTime.Now} - {result.Message}";
+            }
+            else
+            {
+                return $"{DateTime.Now} - {result.Message}";
+            }
+        }
+
+    }
+}

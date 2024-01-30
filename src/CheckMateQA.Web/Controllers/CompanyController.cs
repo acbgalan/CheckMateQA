3748335dc91090a28c2d9f67a5a6db0d83374458
Carlos Blanco <acbgalan@gmail.com>
2024-01-30 02:21:12 +0100

    CompanyController. Basic index action and view

---
+++ src/CheckMateQA.Web/Controllers/CompanyController.cs
@@ -0,0 +1,23 @@
+﻿using CheckMateQA.DataAccess.Data;
+using CheckMateQA.DataAccess.Repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheckMateQA.Web.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyRepository _companyRepository;
+
+        public CompanyController(ICompanyRepository companyRepository)
+        {
+            _companyRepository = companyRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var companies = await _companyRepository.GetAllAsync();
+
+            return View(companies);
+        }
+    }
+}

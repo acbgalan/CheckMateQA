52139241a356c64c1fca6885f111631af61d0063
Carlos Blanco <acbgalan@gmail.com>
2024-02-01 11:08:00 +0100

    CompanyController/Edit. Basic functionality"

--- src/CheckMateQA.Web/Controllers/CompanyController.cs
+++ src/CheckMateQA.Web/Controllers/CompanyController.cs
@@ -3,6 +3,7 @@ using CheckMateQA.DataAccess.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
 using CheckMateQA.Models;
+using System.Data;
 
 namespace CheckMateQA.Web.Controllers
 {
@@ -51,6 +52,50 @@ namespace CheckMateQA.Web.Controllers
             return RedirectToAction("Index", "Company");
         }
 
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var company = await _companyRepository.GetAsync((int)id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return View(company);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Company company)
+        {
+            if (id != company.Id)
+            {
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(company);
+            }
+
+            await _companyRepository.UpdateAsync(company);
+            int saveResult = await _companyRepository.SaveASync();
+
+            if (saveResult > 0)
+            {
+                TempData["success"] = "Empresa creado con exito";
+            }
+            else
+            {
+                TempData["error"] = "Error al crear nueva empresa";
+            }
+
+            return RedirectToAction("Index", "Company");
+        }
     }
 }
